a88b21b5270812efafdee4a9bb6f30912b905e3e
devasrik <140009581+devasrik@users.noreply.github.com>
2026-02-24 20:43:37 +0000

    Added the new functionalities

--- src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -14,85 +14,49 @@ public class Portfolio {
     private long portfolioId;
 
     @Column(nullable = false)
-    private String firstName;
+    private LocalDate creationDate;
 
-    @Column(nullable = false)
-    private String lastName;
+    @OnetoOne
+    private Client client;
 
-    @Column(nullable = false)
-    private String address;
-
-    @Column(nullable = false)
-    private String phone;
-
-    @Column(nullable = false)
-    private String email;
-
-    @ManytoOne // added this for relationship type with advisor and client 
-    private FinancialAdvisor advisor;
+    @ManytoOne(mappedBy = "portfolio") // added this for relationship type with advisor and client 
+    private Set<Security> securities;
 
     protected Portfolio() {
 
     }
 
-    public Portfolio(String firstName, String lastName, String address, String phone, String email) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.phone = phone;
-        this.email = email;
-        this.advisor = advisor; // added this for advisor 
+    public Portfolio(LocalDate creationDate, Client client, Set<Security> securities) {
+        this.creationDate = creationDate;
+        this.client = client;
+        this.securities = securities;
     }
 
-    public Long getClientId() {
-        return clientId;
+   public Long getPortfolioId() {
+        return portfolioId;
     }
 
-    public String getFirstName() {
-        return firstName;
+    public LocalDate getCreationDate() {
+        return creationDate;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    public void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
     }
 
-    public String getLastName() {
-        return lastName;
+    public Client getClient() {
+        return client;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    public void setClient(Client client) {
+        this.client = client;
     }
 
-    public String getAddress() {
-        return address;
+    public Set<Security> getSecurities() {
+        return securities;
     }
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public FinancialAdvisor getAdvisor() {
-        return advisor;
-    }
-
-    public setAdvisor(FinancialAdvisor advisor) {
-        this.advisor = advisor;
+    public void setSecurities(Set<Security> securities) {
+        this.securities = securities;
     }
 }
